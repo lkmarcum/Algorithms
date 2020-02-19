1d341ff8e25b7ad8a8aaff6bcb7ebf1e4473069f
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2020-02-19 15:50:15 -0600

    created first-pass solution for eating_cookies; refactored to optimize for large inputs

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,14 +3,35 @@
 import sys
 
 # The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
+# a solution that is more efficient than the naive
 # recursive solution
+
+
 def eating_cookies(n, cache=None):
-  pass
+    # hard-coded starter list for n = 0, 1, 2
+    permutations = [1, 1, 2]
+
+    # pull from starter list if n is too small
+    if n <= 2:
+        total_perms = permutations[n]
+
+    # update starter list to represent calcs for n-1, n-2, n-3 for each n
+    else:
+        for i in range(3, n):
+            permutations.append(
+                permutations[0] + permutations[1] + permutations[2])
+            # drop value for n-4 (not needed to calc)
+            permutations.pop(0)
+        # sum list to get result for n
+        total_perms = sum(permutations)
+
+    return total_perms
+
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    if len(sys.argv) > 1:
+        num_cookies = int(sys.argv[1])
+        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
+            ways=eating_cookies(num_cookies), n=num_cookies))
+    else:
+        print('Usage: eating_cookies.py [num_cookies]')
