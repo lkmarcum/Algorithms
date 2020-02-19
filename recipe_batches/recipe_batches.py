e2d4b65b42750c92d4ff591a49bc472d6b0b3d34
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2020-02-19 14:55:29 -0600

    completed first-pass solution for recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,22 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    smallest_ratios = []
+    for key in recipe:
+        if not key in ingredients:
+            return 0
+        else:
+            smallest_ratios.append(ingredients[key] // recipe[key])
+    print(smallest_ratios)
+    return min(smallest_ratios)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5}
+    ingredients = {'milk': 1288, 'flour': 9, 'sugar': 95}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
