bf3db770b85b183cb775e789fd57c97bc096a98b
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2020-02-20 15:12:21 -0600

    completed first-pass solution for rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,27 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    plays = ['rock', 'paper', 'scissors']
+    permutations = []
+
+    def recurse(current_n, perm_list):
+        if current_n == 0:
+            permutations.append(perm_list)
+            return
+
+        for i in range(0, len(plays)):
+            recurse(current_n - 1, perm_list + [plays[i]])
+
+    recurse(n, [])
+
+    return permutations
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
